4c6fe6f7f3d446ae508f2d4d3fe4c450eb8873d2
Satyam <bajpai1973satyam@gmail.com>
2024-02-14 00:11:58 +0530

    feat: connect frontend to backend with basic health api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,29 @@
-import express from "express";
+require("dotenv").config();
+import express, { Request, Response } from "express";
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const dateInUnix = Date.now();
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.get("/", (req, res) => {
-  const responseString: string = "Hello World";
-  res.send(responseString);
+app.use(express.json({ type: "application/json" }));
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "API is working fine!" });
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  const currentDateInUnix: number = Number(
+    ((Date.now() - dateInUnix) / 1000).toFixed(2)
+  );
+  res.json({ uptime: currentDateInUnix });
 });
 
 app.listen(port, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
+const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export default function App() {
+  console.log(BACKEND_BASE_URL);
+  useEffect(() => {
+    async function getHealthOfAPI() {
+      const healthResponse = await fetch(`${BACKEND_BASE_URL}/health`);
+      const healthData = await healthResponse.json();
+
+      console.log(healthData);
+    }
+    getHealthOfAPI();
+  }, []);
   return <h1>Hello from React app!</h1>;
 }
